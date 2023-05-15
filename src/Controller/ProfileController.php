42719d7aaf52203eb6f5ad3ec9749c3a856f2dec
Ramzi Mejri <mejrira82@gmail.com>
2023-05-15 12:54:55 +0100

    Controller creation

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/account', name: 'account_')]
+class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'Account',
+        ]);
+    }
+    #[Route('/orders', name: 'orders')]
+    public function orders(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'Orders',
+        ]);
+    }
+}
