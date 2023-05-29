3a841bfb89b05f01823c1e3b4ba7b374dd8401ea
Ramzi Mejri <mejrira82@gmail.com>
2023-05-29 16:35:16 +0100

    Admin interface

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/users', name: 'admins_users_')]
+#[Route('/admin/users', name: 'admin_users_')]
 class UsersController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(UsersRepository $usersRepository): Response
     {
-        return $this->render('admin/users/index.html.twig');
+        $users=$usersRepository->findAll();
+        return $this->render('admin/users/index.html.twig',compact('users'));
     }
 }
